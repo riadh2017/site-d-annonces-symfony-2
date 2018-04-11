2735e023f8db9d6aacb928309b22d93dad8259a6
riadh <riad.mi.2012@gmail.com>
2018-04-11 15:39:55 +0200

    formulaire multipe imbriquer 'category'

--- src/ri/PlatformeBundle/Form/AdvertType.php
+++ src/ri/PlatformeBundle/Form/AdvertType.php
@@ -10,7 +10,11 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use ri\PlatformeBundle\Entity\Advert;
+use ri\PlatformeBundle\Entity\Category;
+use Doctrine\Common\Collections\ArrayCollection;
 
 
 
@@ -23,13 +27,23 @@ class AdvertType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
 $builder
-      ->add('date'   ,DateType::class,['label'=>'date creation'])
+      ->add('date'   ,DateType::class,['label'=>'date'])
       ->add('title' ,TextType::class )
       ->add('content'  ,TextareaType::class )
       ->add('author' , TextType::class)
       ->add('published' ,CheckboxType::class)
       ->add('image', ImageType::class )
-      ->add('save',SubmitType::class )
+      ->add('categories', EntityType::class, array(
+  'class'    => Category::class,
+  'choice_label'=> 'name',
+  'multiple' => false))
+
+
+      // pour permettre de saisir des category
+      /*->add('categories',CollectionType::class,array('entry_type'=>  CategoryType::class, 'allow_add'=>true,'allow_delete'=>true  ))*/
+            
+    
+      ->add('save',SubmitType::class)
       //->setAction("home/test")
     ;
     }
