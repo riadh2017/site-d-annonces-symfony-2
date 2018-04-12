12edf63a2f5b14ce49dadc8bf06f51f3761ae254
riadh <riad.mi.2012@gmail.com>
2018-04-12 17:36:06 +0200

    une condition sur les retour de formulaire avec defind

--- src/ri/PlatformeBundle/Form/AdvertType.php
+++ src/ri/PlatformeBundle/Form/AdvertType.php
@@ -15,6 +15,8 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use ri\PlatformeBundle\Entity\Advert;
 use ri\PlatformeBundle\Entity\Category;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 
 
@@ -31,7 +33,6 @@ $builder
       ->add('title' ,TextType::class )
       ->add('content'  ,TextareaType::class )
       ->add('author' , TextType::class)
-      ->add('published' ,CheckboxType::class)
       ->add('image', ImageType::class )
       ->add('categories', EntityType::class, array(
                'class'        => Category::class,
@@ -45,8 +46,33 @@ $builder
       ->add('save',SubmitType::class)
       //->setAction("home/test")
     ;
+
+
+
+     // On ajoute une fonction qui va écouter un évènement
+    $builder
+    ->addEventListener(
+      FormEvents::PRE_SET_DATA,    // 1er argument : L'évènement qui nous intéresse : ici, PRE_SET_DATA
+      function(FormEvent $event) { // 2e argument : La fonction à exécuter lorsque l'évènement est déclenché
+        // On récupère notre objet Advert sous-jacent
+        $advert = $event->getData();
+
+        // Cette condition est importante, on en reparle plus loin
+        if (null === $advert) {
+          return; // On sort de la fonction sans rien faire lorsque $advert vaut null
+        }
+
+        if (!$advert->getPublished() || null == $advert->getId()) {
+          // Si l'annonce n'est pas publiée, ou si elle n'existe pas encore en base (id est null),
+          // alors on ajoute le champ published
+          $event->getForm()->add('published', CheckboxType::class, array('required' => false));
+        } else {
+          // Sinon, on le supprime
+          $event->getForm()->remove('published');
+        }
     }
-     
+     );
+  }
 
     /**
      * {@inheritdoc}
